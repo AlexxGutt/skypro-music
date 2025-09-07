a3ba1543133b15748b0f139cc4b1b45677962dd0
AlexxGutt <alex.gutt89@gmail.com>
2025-09-07 19:56:23 +0300

    Разбил на оставшиеся компоненты

--- src/app/components/Centerblock/Centerblock.tsx
+++ src/app/components/Centerblock/Centerblock.tsx
@@ -1,76 +1,18 @@
 import styles from './centerblock.module.css';
-import Link from 'next/link';
 import classnames from 'classnames';
 import Search from '../Search/Search';
-import { data } from '@/app/data';
-import { formatTime } from '@/app/utils/helper';
+import Track from '../Track/Track';
+import Filter from '../Filter/Filter';
+import FilterItem from '../FilterItem/FilterItem';
 export default function Centerblock() {
   return (
     <div className={styles.centerblock}>
       <Search />
       <h2 className={styles.centerblock__h2}>Треки</h2>
-      <div className={styles.centerblock__filter}>
-        <div className={styles.filter__title}>Искать по:</div>
-        <div className={styles.filter__button}>исполнителю</div>
-        <div className={styles.filter__button}>году выпуска</div>
-        <div className={styles.filter__button}>жанру</div>
-      </div>
+      <Filter />
       <div className={styles.centerblock__content}>
-        <div className={styles.content__title}>
-          <div className={classnames(styles.playlistTitle__col, styles.col01)}>
-            Трек
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col02)}>
-            Исполнитель
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col03)}>
-            Альбом
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col04)}>
-            <svg className={styles.playlistTitle__svg}>
-              <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
-            </svg>
-          </div>
-        </div>
-        <div className={styles.content__playlist}>
-          <div className={styles.playlist__item}>
-            {data.map((track) => (
-              <div key={track._id} className={styles.playlist__track}>
-                <div className={styles.track__title}>
-                  <div className={styles.track__titleImage}>
-                    <svg className={styles.track__titleSvg}>
-                      <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
-                    </svg>
-                  </div>
-                  <div>
-                    <Link className={styles.track__titleLink} href="">
-                      {track.name}
-                      <span className={styles.track__titleSpan}></span>
-                    </Link>
-                  </div>
-                </div>
-                <div className={styles.track__author}>
-                  <Link className={styles.track__authorLink} href="">
-                    {track.author}
-                  </Link>
-                </div>
-                <div className={styles.track__album}>
-                  <Link className={styles.track__albumLink} href="">
-                    {track.album}
-                  </Link>
-                </div>
-                <div className="track__time">
-                  <svg className={styles.track__timeSvg}>
-                    <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
-                  </svg>
-                  <span className={styles.track__timeText}>
-                    {formatTime(track.duration_in_seconds)}
-                  </span>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <FilterItem />
+        <Track />
       </div>
     </div>
   );
